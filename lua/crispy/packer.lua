3e58131d78d2035312b70ffb1ea7c1ccdd1e44a4
Lee seung chan <dltmdcks702@gmail.com>
2023-06-10 16:00:48 +0900

    reset nvim tree

--- lua/crispy/packer.lua
+++ lua/crispy/packer.lua
@@ -6,6 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'rajasegar/vim-pnpm'
 
   use {
     'nvim-tree/nvim-web-devicons',
